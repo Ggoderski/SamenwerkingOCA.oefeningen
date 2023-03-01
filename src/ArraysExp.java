a86eb23932eac46bf4f396ce836d08a86a67e388
Your Name <you@example.com>
2023-03-01 16:01:13 +0100

    1maartpt3

---
+++ src/ArraysExp.java
@@ -0,0 +1,19 @@
+
+
+public class ArraysExp {
+	
+	
+	static int[][] p1 = {{1,2,3,4},{5,6,7,8}};
+	
+	static int ArrayVoorbeeld() {
+	return p1[1][3] = 17;
+	
+	}
+	
+	public static void main(String[] args) {
+		ArrayVoorbeeld();
+	System.out.println(p1[1][3]);
+	}
+	
+	}
+
