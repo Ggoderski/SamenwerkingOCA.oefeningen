51c0a814d9663cb329a68e2cfda6986e3c78cdb1
Your Name <you@example.com>
2023-03-01 14:46:56 +0100

    1maartpt2

---
+++ src/FieldArgumentReturnExp3.java
@@ -0,0 +1,22 @@
+
+public class FieldArgumentReturnExp3 {
+	
+	static void Getleeftijd(int leeftijd) {
+		
+		if (leeftijd < 21) {
+			System.out.println("je bent te jong");
+			
+			
+		}else {
+			System.out.println("kom maar naar binnen");
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		Getleeftijd(23);
+	}
+	
+	
+
+}
